cd9d30bbf80073e426575d65d8b7bfb03a97df0e
ArtemKamenskiy <tema_111@ukr.net>
2018-12-09 10:34:09 +0300

    added 5, 6 and 7 levels (end game)

--- homework 7/robot.js
+++ homework 7/robot.js
@@ -1,18 +1,144 @@
+// start game
+
 // 1 level
 for( var i = 0; i<2; i++){
     north();
 }
+
 // 2 level
 for( var i = 0; i<16; i++){
     east();
 }
+
 // 3 level
 for( var i = 0; i<7; i++){
     south();
     east();
 }
+
 // 4 level
-for( var i = 0; i<=7; i++){
+for( var i = 0; i<10; i++){
     south();
     east();
 }
+// or
+for( var i = 0; i<1; i++){
+    south();
+}
+for( var i = 0; i<=2; i++){
+    east();
+}
+for( var i = 0; i<=1; i++){
+    south();
+}
+for( var i = 0; i<1; i++){
+    east();
+}
+for( var i = 0; i<=2; i++){
+    south();
+}
+for( var i = 0; i<=1; i++){
+    east();
+}
+for( var i = 0; i<1; i++){
+    south();
+}
+
+// 5 level
+for( var i = 0; i<=5; i++){
+    east();
+}
+for( var i = 0; i<=1; i++){
+    south();
+}
+for( var i = 0; i<=1; i++){
+    west();
+}
+for( var i = 0; i<=2; i++){
+    south();
+}
+for( var i = 0; i<=1; i++){
+    west();
+}
+for( var i = 0; i<=2; i++){
+    north();
+}
+for( var i = 0; i<=1; i++){
+    west();
+}
+for( var i = 0; i<=4; i++){
+    south();
+}
+for( var i = 0; i<=5; i++){
+    east();
+}
+
+// 6 level
+for( var i = 0; i<=4; i++){
+    south();
+}
+for( var i = 0; i<=1; i++){
+    east();
+}
+for( var i = 0; i<=2; i++){
+    north();
+}
+for( var i = 0; i<1; i++){
+    east();
+}
+for( var i = 0; i<=1; i++){
+    north();
+}
+for( var i = 0; i<=2; i++){
+    east();
+}
+for( var i = 0; i<=3; i++){
+    south();
+}
+for( var i = 0; i<=1; i++){
+    west();
+}
+for( var i = 0; i<=2; i++){
+    south();
+}
+for( var i = 0; i<=1; i++){
+    east();
+}
+
+// 7 level; 
+for( var i = 0; i<=10; i++){
+    south();
+}
+for( var i = 0; i<=3; i++){
+    east();
+}
+for( var i = 0; i<=10; i++){
+    north();
+}
+for( var i = 0; i<=1; i++){
+    east();
+}
+for( var i = 0; i<1; i++){
+    south();
+    east();
+}
+for( var i = 0; i<=4; i++){
+    south();
+}
+for( var i = 0; i<1; i++){
+    west();
+}
+for( var i = 0; i<=6; i++){
+    south();
+}
+for( var i = 0; i<=7; i++){
+    east();
+}
+for( var i = 0; i<=8; i++){
+    south();
+}
+for( var i = 0; i<1; i++){
+    east();
+}
+
+// end game
