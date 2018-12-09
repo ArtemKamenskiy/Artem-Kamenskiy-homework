e89e01b62f8dc6038abb10d37997491138c1b2e0
ArtemKamenskiy <tema_111@ukr.net>
2018-12-09 07:28:38 +0300

    added homework 7

---
+++ homework 7/robot.js
@@ -0,0 +1,18 @@
+// 1 level
+for( var i = 0; i<2; i++){
+    north()
+}
+// 2 level
+for( var i = 0; i<16; i++){
+    east()
+}
+// 3 level
+for( var i = 0; i<7; i++){
+    south()
+    east()
+}
+// 4 level
+for( var i = 0; i<=7; i++){
+    south()
+    east()
+}
