080610087752e108d181f14a2c8fbb5170331183
ArtemKamenskiy <tema_111@ukr.net>
2019-01-24 08:32:32 +0300

    fix

--- homework9/main.js
+++ homework9/main.js
@@ -1,65 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
-function random(min, max) {
-	return Math.floor(Math.random() * (max - min)) + min;
-}
+    function random(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
-let rand = random(0, 11);
+    let rand = random(0, 11);
 
-setTimeout(function() {
-	console.log(rand);
-} ,1000)
-
-
-setTimeout(function() {
-	console.log(rand);
-} ,2000)
-
-setTimeout(getPostByCallback, 2500);
-setTimeout(getPostByPromise, 3000);
-
-//getPostByCallback
+    setTimeout(function() {
+        console.log(rand);
+    }, 1000)
 
 
-function getPostByCallback(){
-	function post(callback){
-		let xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if (this.readyState ===  4 &&  this.status === 200) {
-			callback(this.responseText);
-		}
-	};
-	xhttp.open("GET", "http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}");
-	xhttp.send();
-	}
+    setTimeout(function() {
+        console.log(rand);
+    }, 2000)
 
-	post(function(responseText){
-		console.log(JSON.parse(responseText));
-	});
-
-	};
+    setTimeout(getPostByCallback, 2500);
+    setTimeout(getPostByPromise, 3000);
 
 
-//getPostByPromise
+    //getPostByCallback
+    function getPostByCallback() {
+        function post(callback) {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    callback(this.responseText);
+                }
+            };
+            xhttp.open("GET", "http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}");
+            xhttp.send();
+        }
 
-function getPostByPromise(){
-	let myRequest = new Promise((resolve, reject) => {
-		let xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if (this.readyState ===  4 &&  this.status === 200) {
-			resolve(this.responseText);
-			reject(this.status);
-		}
-	};
-	xhttp.open("GET", "http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}");
-	xhttp.send();
-	});
+        post(function(responseText) {
+            console.log(JSON.parse(responseText));
+        });
 
-	myRequest.then((responseText) => {
-		console.warn(JSON.parse(responseText))
-	}).catch((status) =>{
-		console.warn(status)
-	});
-	};
+    };
+
+
+    //getPostByPromise
+    function getPostByPromise() {
+        let myRequest = new Promise((resolve, reject) => {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    resolve(this.responseText);
+                    reject(this.status);
+                }
+            };
+            xhttp.open("GET", "http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}");
+            xhttp.send();
+        });
+
+        myRequest.then((responseText) => {
+            console.warn(JSON.parse(responseText))
+        }).catch((status) => {
+            console.warn(status)
+        });
+    };
 
 
 });
